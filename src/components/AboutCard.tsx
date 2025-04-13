1a5d882930fcf0bf124443c675ccce8dac997e79
Zavier Miller <zaviermiller@github.com>
2025-04-13 13:58:44 -0500

    fix styling issue

--- src/components/AboutCard.tsx
+++ src/components/AboutCard.tsx
@@ -18,13 +18,15 @@ export default function AboutCard({
   linkedIn,
 }: AboutCardProps) {
   return (
-    <div className="flex flex-col items-start justify-center gap-8 border border-pa-primary border-opacity-20 p-6 sm:w-96 bg-white bg-opacity-40">
+    <div className="flex flex-col items-start gap-8 border border-pa-primary border-opacity-20 p-6 sm:w-96 bg-white bg-opacity-40">
       <div className="flex gap-4 items-center">
         <img src={imageUrl} className="rounded-full w-24" />
         <div className="flex flex-col items-start justify-center gap-2">
           <div>
-            <h2 className="text-2xl font-serif font-semibold">{name}</h2>
-            <p className="text-xl text-pa-primary opacity-75">{title}</p>
+            <h2 className="text-xl font-serif font-semibold">{name}</h2>
+            <p className="text-lg leading-tight text-pa-primary opacity-75">
+              {title}
+            </p>
           </div>
           <span className="flex gap-2">
             <a href={linkedIn} target="_blank">
